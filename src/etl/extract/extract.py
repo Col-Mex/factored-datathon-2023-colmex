4603aaf1dca4b923a12f1e7eb49233ee43bc42fa
Jose Nino <jdninom@unal.edu.co>
2023-07-26 23:52:57 -0700

    Updated extract.py to dowload all compressed files

--- src/etl/extract/extract.py
+++ src/etl/extract/extract.py
@@ -1,68 +1,50 @@
 import gzip
 import json
+import joblib
+import os
 from azure.storage.filedatalake import DataLakeServiceClient, DataLakeFileClient
 from azure.core.credentials import AzureSasCredential
-
+import time
 
 storage_account_name = "safactoreddatathon"
 container_name = "source-files"
 sas_token = "sp=rle&st=2023-07-25T18:12:36Z&se=2023-08-13T02:12:36Z&sv=2022-11-02&sr=c&sig=l2TCTwPWN8LSM922lR%2Fw78mZWQK2ErEOQDUaCJosIaw%3D"
-file_path = "amazon_reviews"
 service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
     "https", storage_account_name), credential=sas_token)
 file_system_client = service_client.get_file_system_client(file_system=container_name)
 
-# List all the files in the directory
-paths = file_system_client.get_paths(file_path)
+def download_compressed_data(folder_to_download, path_to_save, name_joblib_downloaded):
+    print(f"Started downloading from {folder_to_download}")
+    start_time = time.time()
+    paths = file_system_client.get_paths(folder_to_download)
 
-counter = 0
-all_data = []
+    counter = 0
+    # Check if joblib exists with a list of names of files already downloaded. If not, create an empty list
+    joblib_dreviews = os.path.join(path_to_save, name_joblib_downloaded)
+    if os.path.exists(joblib_dreviews):
+        downloaded_data = joblib.load(joblib_dreviews)
+    else:
+        downloaded_data = []
 
-for path in paths:
-    if not path.is_directory and path.name.endswith('.json.gz'):
-        file_client = file_system_client.get_file_client(path.name)
-        file_content = file_client.download_file().readall()
-        
-        # Decompress the gzip file and decode the bytes to string
-        decompressed_data = gzip.decompress(file_content).decode('utf-8')
-        
-        # Each line contains a separate JSON object
-        json_objects = [json.loads(line) for line in decompressed_data.splitlines() if line]
+    for path in paths:
+        path_name = path.name
+        total_path = os.path.join(path_to_save, path_name)
+        if path_name.endswith('.json.gz')  and path_name not in downloaded_data:
+            file_client = file_system_client.get_file_client(path_name)
+            os.makedirs(os.path.dirname(total_path), exist_ok=True)
+            with open(total_path, "wb") as download_file:
+                    data = file_client.download_file().readall()
+                    download_file.write(data)
+                    downloaded_data.append(path_name)
+                    joblib.dump(downloaded_data, joblib_dreviews)
+            counter += 1
+            if counter % 2 == 0:
+                print(f"Downloaded {counter} files until now. Took {time.time() - start_time} seconds")
+            if counter == 5:
+                break
+    hours, rem = divmod(time.time() - start_time, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print(f"{counter} files downloaded in {int(hours)}:{int(minutes)}:{seconds} (h:m:s)")
 
-        # Save the json data to a local file
-        file_name = 'reviews' + str(counter) + '.json'
-        with open(file_name, 'w') as json_file:
-            for json_obj in json_objects:
-                json.dump(json_obj, json_file)
-                json_file.write('\n')
-        counter += 1
-        if counter >=1:
-            break
-
-file_path = "amazon_metadata"
-# List all the files in the directory
-paths = file_system_client.get_paths(file_path)
-
-counter = 0
-all_data = []
-
-for path in paths:
-    if not path.is_directory and path.name.endswith('.json.gz'):
-        file_client = file_system_client.get_file_client(path.name)
-        file_content = file_client.download_file().readall()
-        
-        # Decompress the gzip file and decode the bytes to string
-        decompressed_data = gzip.decompress(file_content).decode('utf-8')
-        
-        # Each line contains a separate JSON object
-        json_objects = [json.loads(line) for line in decompressed_data.splitlines() if line]
-
-        # Save the json data to a local file
-        file_name = 'metadata' + str(counter) + '.json'
-        with open(file_name, 'w') as json_file:
-            for json_obj in json_objects:
-                json.dump(json_obj, json_file)
-                json_file.write('\n')
-        counter += 1
-        if counter >=1:
-            break
+download_compressed_data("amazon_metadata", "src/etl/extract", "downloaded_metadata.joblib")
+download_compressed_data("amazon_reviews", "src/etl/extract", "downloaded_reviews.joblib")
